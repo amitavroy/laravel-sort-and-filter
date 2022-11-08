bdab394080def808cc4c2f5af23d8425a0f2c967
manotheus <matheusdelimagomes@gmail.com>
2022-11-08 14:07:57 -0300

    feat: enable a option for user choose a input name.

--- config/sort-and-filter.php
+++ config/sort-and-filter.php
@@ -2,6 +2,16 @@
 
 return [
     /*
+     * Choose the field on which you want to perform operations. 
+     * This option is useful for systems that use camelCase or Snake_case middleware for option.
+     */
+    'sort-by-field'         => 'sortBy',
+    'sort-order-field'      => 'sortOrder',
+    'filter-by-field'       => 'filterBy',
+    'filter-value-field'    => 'filterValue',
+    'search-by-field'       => 'searchBy'
+    'search-value-field'    => 'searchValue'
+    /*
      * This message will be displayed as an error when the field
      * for sorting in request doesn't match with the array
      * defined inside the model.
--- src/SortAndFilter.php
+++ src/SortAndFilter.php
@@ -8,47 +8,47 @@ trait SortAndFilter
 {
     public function scopeSort($query, Request $req)
     {
-        $query->when($req->has('sortBy'), function ($query) use ($req) {
+        $query->when($req->has(config("sort-and-filter.sort-by-field")), function ($query) use ($req) {
             if ($this->sortable !== null && count($this->sortable) !== 0) {
-                if (! in_array($req->input('sortBy'), $this->sortable)) {
+                if (! in_array($req->input(config("sort-and-filter.sort-by-field")), $this->sortable)) {
                     $message = config("sort-and-filter.sort-not-allowed");
                     abort(400, $message);
                 }
             }
 
-            if ($req->has('sortOrder')) {
-                $query->orderBy($req->input('sortBy'), $req->input('sortOrder'));
+            if ($req->has(config("sort-and-filter.sort-order-field"))) {
+                $query->orderBy($req->input(config("sort-and-filter.sort-by-field")), $req->input(config("sort-and-filter.sort-order-field")));
             } else {
-                $query->orderBy($req->input('sortBy'), 'asc');
+                $query->orderBy($req->input(config("sort-and-filter.sort-by-field")), 'asc');
             }
         });
     }
 
     public function scopeFilter($query, Request $req)
     {
-        $query->when(($req->has('filterBy') && $req->has('filterValue')), function ($query) use ($req) {
+        $query->when(($req->has(config("sort-and-filter.filter-by-field")) && $req->has('config("sort-and-filter.filter-value-field")')), function ($query) use ($req) {
             if ($this->filterable !== null && count($this->filterable) !== 0) {
-                if (! in_array($req->input('filterBy'), $this->filterable)) {
+                if (! in_array($req->input(config("sort-and-filter.filter-by-field")), $this->filterable)) {
                     $message = config("sort-and-filter.filter-not-allowed");
                     abort(400, $message);
                 }
             }
 
-            $query->where($req->input('filterBy'), $req->input('filterValue'));
+            $query->where($req->input(config("sort-and-filter.filter-by-field")), $req->input('config("sort-and-filter.filter-value-field")'));
         });
     }
 
     public function scopeSearch($query, Request $req)
     {
-        $query->when(($req->has('searchBy') && $req->has('searchValue')), function ($query) use ($req) {
+        $query->when(($req->has(config("sort-and-filter.search-by-field")) && $req->has(config("sort-and-filter.search-value-field"))), function ($query) use ($req) {
             if ($this->searchable !== null && count($this->searchable) !== 0) {
-                if (! in_array($req->input('searchBy'), $this->searchable)) {
+                if (! in_array($req->input(config("sort-and-filter.search-by-field")), $this->searchable)) {
                     $message = config("sort-and-filter.search-not-allowed");
                     abort(400, $message);
                 }
             }
 
-            $query->where($req->input('searchBy'), 'like', "{$req->input('searchValue')}%");
+            $query->where($req->input(config("sort-and-filter.search-by-field")), 'like', "{$req->input(config("sort-and-filter.search-value-field"))}%");
         });
     }
 }
